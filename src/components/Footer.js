0fc5ca2f639c2bec427f6442dd2ae42ec097862d
XWilliam <william023111@gmail.com>
2021-10-30 17:27:13 -0500

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => (
+  <footer>
+    <div>
+      Creado por <a href="">XWilliam</a>
+    </div>
+    <div>
+      <a href="">Siguenos en Instagram</a>
+    </div>
+  </footer>
+);
+
+export default Footer;
